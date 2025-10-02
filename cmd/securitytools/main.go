2bccf7604213e677a562981166babb66f9b8ec99
Mary <135226561+mpermar082@users.noreply.github.com>
2025-10-02 19:17:14 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/securitytools/main.go
@@ -0,0 +1,23 @@
+// cmd/securitytools/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "securitytools/internal/securitytools"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := securitytools.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
